afb3bf58e14592b24347cf57f65ce57dddf1e9a3
noamtamari <noamtamari98@gmail.com>
2025-01-19 10:25:25 +0200

    merging ConnectionIMP with frame IMPL

--- server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -6,8 +6,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 
-public class ConnectionsImpl implements Connections<T> {
+public class ConnectionsImpl<T> implements Connections<T> {
     private ConcurrentHashMap<Integer, ConnectionHandler<T>> connections;
+    private ConcurrentHashMap<String,  ConcurrentHashMap<Integer, ConnectionHandler<T>>> channels;//map of channels to connections
 
     public ConnectionsImpl() {
         connections = new ConcurrentHashMap<>();
@@ -15,20 +16,31 @@ public class ConnectionsImpl implements Connections<T> {
 
     @Override
     public boolean send(int connectionId, T msg) {
-
+        ConnectionHandler<T> connection = connections.get(connectionId);
+        if (connection != null) {
+            connection.send(msg);
+            return true;
+        }
+        return false;
     }
 
+    // Sends a message T to clients subscribed to the channel
     @Override
     public void send(String channel, T msg) {
-        // IMPLEMENT IF NEEDED
+        ConcurrentHashMap<Integer, ConnectionHandler<T>> channelConnections = channels.get(channel);
+        if (channelConnections != null && !channelConnections.isEmpty()) { //if there are cleints subscribed to the channel or the channel exists
+            for (ConnectionHandler<T> connection : channelConnections.values()) {
+                connection.send(msg);
+            }
+        }
     }
 
     @Override
     public void disconnect(int connectionId) {
-        // IMPLEMENT IF NEEDED
+        connections.remove(connectionId);
     }
 
-    public void connect(int connectionId) {
-        // IMPLEMENT IF NEEDED
-    }
+    // public void connect(int connectionId) {
+    //     // IMPLEMENT IF NEEDED
+    // }
 }
