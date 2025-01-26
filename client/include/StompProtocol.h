75d487c266d7d96c5072f8e2bb3d5be1a6d6413f
noyseg <n3segal@gmail.com>
2025-01-26 18:56:37 +0200

    No checking prints in server

--- client/include/StompProtocol.h
+++ client/include/StompProtocol.h
@@ -56,5 +56,6 @@ public:
     const string summerize_description(const string &string);
     const string epoch_to_date(const string &date_and_time);
     void exportEventsToFile(const string &channel,const string &user,const string &filename);
+    void exportEmptyFile (const string &channel, const string &filename);
     void printSummary(const std::unordered_map<std::string, std::unordered_map<std::string, std::vector<Event>>>& summary);
 };
